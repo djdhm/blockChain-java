4b75792bcdacb2f7f34c73151c7e0e0cc696b834
djamel <ed_dahmane@esi.dz>
2018-09-18 17:07:22 +0200

    To fix: Recherche Mot returning Null

--- src/Client.java
+++ src/Client.java
@@ -1,41 +1,43 @@
 import sun.nio.ch.ThreadPool;
 
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 
 public class Client {
 
-    public static MiningCountdown countdown = new MiningCountdown(1);
-    public static ArrayList<RemoteInterface> noeuds=new ArrayList<>();//La liste des noeuds
-    public static void main() {
+
+    public static void main(String[] argv) {
         try {
-            Remote r = Naming.lookup("rmi://localhost:1099/Noeud1");
-            Remote r2 = Naming.lookup("rmi://localhost:1099/Noeud2");
-            Remote r3 = Naming.lookup("rmi://localhost:1099/Noeud3");
+            RemoteServeur serveur=(RemoteServeur) Naming.lookup(Noeud.ADDRESSE_SERVEUR);
+            System.out.println("Test ajout");
+            serveur.ajouterData("Test data");
+            serveur.ajouterData("Test mouna");
+            serveur.ajouterData("mouna data");
+            serveur.ajouterData("texte test");
+            serveur.ajouterData("Mouna mouna ");
+            serveur.ajouterData("bloque finale");
 
-            RemoteInterface stub1 = (RemoteInterface) r;
-            RemoteInterface stub2=(RemoteInterface) r2;
-            RemoteInterface stub3=(RemoteInterface) r3;
-            noeuds.add(stub1);
-            noeuds.add(stub2);
-            noeuds.add(stub3);
-            String data="exemple data";
-            for(int i=1;i<4;i++){
-                System.out.println(i);
-                Thread thread = new Thread(new MiningRunnable(i, data, countdown));
-                thread.start();
+            System.out.println("Fin d'ajout ");
+            System.out.println("Recherche de mot ");
 
-            }
-            countdown.await();
-            System.out.println("un noud a terminé le mining du nouveau bloque ");
-            for (RemoteInterface noeud:noeuds){
-                System.out.println("En train d'arreter les autres noeuds");
-                noeud.arreterMining(countdown.getNouveauBloqueMine());
+            Iterator<String> iterator=serveur.occurenceMot("mouna").iterator();
+            System.out.println("Resultat");
+            while (iterator.hasNext()){
+                System.out.println(iterator.next());
             }
 
-        } catch (Exception e) {
+
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
             e.printStackTrace();
         }
     }
